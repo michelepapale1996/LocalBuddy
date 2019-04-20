c50d4eac6cfd76d19d417e50dfe8fdb23fc5dd12
michelepapale1996 <michele.papale@mail.polimi.it>
2019-04-20 11:59:14 +0200

    Update ip.js

--- ip.js
+++ ip.js
@@ -1,3 +1,3 @@
 //export default IP_ADDRESS = 'http:192.168.1.97:3000'
-export default IP_ADDRESS = 'http:192.168.100.4:3000'
-//export default IP_ADDRESS = 'https://localbuddy-backend.herokuapp.com'
+//export default IP_ADDRESS = 'http:192.168.100.4:3000'
+export default IP_ADDRESS = 'https://localbuddy-backend.herokuapp.com'
